54414d935d1ae010fb5fd16471e92745c974c106
anoushka-prabhu <134020219+anoushka-prabhu@users.noreply.github.com>
2023-05-18 19:45:04 -0500

    Add files via upload

---
+++ howdy_world.py
@@ -0,0 +1,12 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         ANOUSHKA PRABHU
+# Section:      213
+# Assignment:   Lab Howdy World
+# Date:         26 August 2022
+#
+
+print("Howdy, World!")
+print("I like to read novels.")
---
+++ leet_speak.py
@@ -0,0 +1,27 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Name:         ANOUSHKA PRABHU
+# Section:      213
+# Assignment:   leet speak
+# Date:         11 October 2022
+#
+
+text = input("Enter some text:")
+words = text.split()
+keys = {'a' : '4', 'e' : '3', 'o' : '0', 's' : '5', 't' : '7'}
+leetlist = []
+#loop for conversions
+for i in words:
+    for j in i :
+        if j in keys :
+            leetlist.append(keys.get(j))
+        else:
+            leetlist.append(j)
+leet = ''.join(leetlist)
+
+print('In leet speak,"' , text, '" is:', leet)
+            
+            
+        
